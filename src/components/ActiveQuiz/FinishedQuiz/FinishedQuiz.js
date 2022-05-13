3bad083931a66ab28862a8252e1f1def48cf0989
Aleh Shchurko <alehshchurko@gmail.com>
2022-05-13 20:55:34 +0300

    add sidebar, lesson ...58

--- src/components/ActiveQuiz/FinishedQuiz/FinishedQuiz.js
+++ src/components/ActiveQuiz/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import React from "react";
-import classes from './FinishedQuiz.module.css'
+import classes from './FinishedQuiz.module.css';
+import Button from "../../UI/Button/Button";
 
 const FinishedQuiz = props => {
 
@@ -36,7 +37,13 @@ const FinishedQuiz = props => {
             <p>Правильно {successCount} из {props.quiz.length}</p>
 
             <div>
-                <button onClick={props.onRetry}>Повторить</button>
+                <Button onClick={props.onRetry} type='primary'>
+                    Повторить
+                </Button>
+
+                <Button type='success'>
+                    Перейти в список тестов
+                </Button>
             </div>
         </div>
     )
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import classes from './Layout.module.css';
+import MenuToogle from '../../components/Navigation/MenuToogle/MenuToogle';
+import Drawer from '../../components/Navigation/Drawer/Drawer';
 
 
 
 class Layout extends Component {
 
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
+    menuCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    }
+
+
     render() {
         return(
             <div className={classes.Layout}>
+
+                <Drawer
+                    isOpen={this.state.menu}
+                    onClose={this.toggleMenuHandler}
+                />
+
+                <MenuToogle
+                    onToogle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
+
                 <main>
                     { this.props.children }
                 </main>
